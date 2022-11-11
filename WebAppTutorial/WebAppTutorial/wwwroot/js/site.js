4ba668d7423a8efc3bb2e25e3795bf4761b67db3
SerengetiJade <jad245@gmail.com>
2022-11-10 16:34:19 -0800

    Add project files.

---
+++ WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function newColor() {
+    document.getElementById('hello_world').style.color = "blue";
+}
+function oldColor() {
+    document.getElementById('hello_world').style.color = "darkolivegreen";
+}
+
+//animate image
+var vw = document.documentElement.clientWidth;
+var vh = document.documentElement.clientHeight;
+
+var id = null;
+function imgAnimation() {
+    var elem = document.getElementById("dragonfly");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
