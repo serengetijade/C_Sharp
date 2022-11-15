6f11303e84d22da5672a56c7ad8d755e1a0e25f6
SerengetiJade <jad245@gmail.com>
2022-11-15 14:19:21 -0800

    Package Express

--- Assignment/Assignment/Program.cs
+++ Assignment/Assignment/Program.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 
-namespace InsuranceApproval
+namespace ShippingQuote
 {
-    class InsApproval
+    class ShippingQuote
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Have you ever had a DUI? Enter true or false");
-            bool DUI = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("How many speeding tickets do you have");
-            int speeding = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Qualified? " + (age >= 15 && DUI == false && speeding <= 3));    //Use AND to compare boolean results
-            Console.ReadLine();
+            Console.WriteLine("Welcome to Package Express.\nPlease follow the instructions below.\nWhat is the package weight?");
+            decimal packWeight = Convert.ToDecimal(Console.ReadLine());
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                //Console.ReadLine() to get the user's package dimensions:
+                Console.WriteLine("What is the package width?");
+                decimal packWidth = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("What is the package height?");
+                decimal packHeight = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("What is the package length?");
+                decimal packLength = Convert.ToDecimal(Console.ReadLine());
+                //Ternary operator (?) to return quote:
+                string quote = (packWidth + packHeight + packLength) > 50 ? "Package too big to be shipped via Package Express." : "Shipping quote: $" + ((packWidth * packHeight * packLength * packWeight) / 100);
+                Console.WriteLine(quote);
+            }
         }
     }
 }
--- CSharpBasics/CSharpBasics/Program.cs
+++ CSharpBasics/CSharpBasics/Program.cs
@@ -201,12 +201,12 @@ namespace InsuranceApproval
         }
     }
 }*/
-namespace ifStatements
+/*namespace ifStatements
 {
     class ifStatements
     {
         static void Main(string[] args)
-        {   /*
+        {   
             if(true)
             {
                 Console.WriteLine("Hello!");
@@ -281,7 +281,7 @@ namespace ifStatements
             {
                 Console.WriteLine("It is not exactly room temperature.");
             } 
-            */
+            
 
             //To write the above with ternary (?) operator:
             int currentTemp = 80;
@@ -295,6 +295,37 @@ namespace ifStatements
             Console.WriteLine(result);
 
             Console.ReadLine();
+        }
+    }
+}*/
+
+namespace ShippingQuote
+{
+    class ShippingQuote
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express.\nPlease follow the instructions below.\nWhat is the package weight?");
+            decimal packWeight = Convert.ToDecimal(Console.ReadLine());
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                //Console.ReadLine() to get the user's package dimensions:
+                Console.WriteLine("What is the package width?");
+                decimal packWidth = Convert.ToDecimal(Console.ReadLine());                  
+                Console.WriteLine("What is the package height?");
+                decimal packHeight = Convert.ToDecimal(Console.ReadLine());                  
+                Console.WriteLine("What is the package length?");
+                decimal packLength = Convert.ToDecimal(Console.ReadLine());
+                //Ternary operator (?) to return quote:
+                string quote = (packWidth + packHeight + packLength) > 50 ? "Package too big to be shipped via Package Express." : "Shipping quote: $" + ((packWidth * packHeight * packLength * packWeight) / 100);
+                Console.WriteLine(quote);
+            }                        
         }
     }
 }
