5c18a4f7da13e1302c6a556eb3d4ea8c93e3eb96
SerengetiJade <jad245@gmail.com>
2022-11-18 17:48:45 -0800

    Exception Handling example 2

--- Basic_CSharp/CSharp_Basics/CSharpBasics/Program.cs
+++ Basic_CSharp/CSharp_Basics/CSharpBasics/Program.cs
@@ -608,4 +608,45 @@ using System.Collections.Generic; //required for Lists
         }
     }
 }  */
-
+namespace ExceptionHandling
+{
+    class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            /*TRY CATCH FINALLY NOTES:
+            try
+            {
+                Console.WriteLine("Picke a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two.....");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals: " + numberThree);
+            }
+            //This will catch when a 'FormatException' occurs, and assigns that to the variable 'ex':
+            //Other exceptions are available, like 'Exception'.
+            catch (FormatException ex)
+            {
+                //Define the message when this exception occurs:
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine("Please type a valid number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide by zero");
+            }
+            //Exception will catch any other exception that is not explicitly defined:
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Code to run regardless of an error or not: 
+            finally
+            {
+                Console.ReadLine();
+            }      
+        }
+    }
+}*/
