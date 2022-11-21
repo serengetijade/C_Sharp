12b1ff77e37bfd84b8c760f3513da1fa6ef7c6ac
SerengetiJade <jad245@gmail.com>
2022-11-21 12:24:37 -0800

    method overloading

--- TwentyOneGame/TwentyOneGame/Program.cs
+++ TwentyOneGame/TwentyOneGame/Program.cs
@@ -25,8 +25,11 @@ namespace TwentyOneGame
 
             //Instantiate a class object, 'deck' of class 'Deck' from Deck.cs. This has a property named 'Cards', which is a list of 52 cards - defined in Deck.cs
             Deck deck = new Deck();
+
+            int timesShuffled = 0;
+
             //Reassign the value of deck/apply the shuffle method to it: 
-            deck = Shuffle(deck);
+            deck = Shuffle(deck: deck , out timesShuffled, times: 3);   //Call the third Shuffle method using NAMED parameters
 
             //Syntax: foreach (className instanceName1 in instanceName2.classConstructor)
             //Syntax breakdown: Card = class defined in Card.cs; card = an instance of class Card as defined in Deck.cs; deck = an instance of class Deck defined above; Cards = list of Card objects as defined in Deck.cs;
@@ -36,26 +39,65 @@ namespace TwentyOneGame
             }
 
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times shuffled: {0}", timesShuffled);
             Console.ReadLine();
         }
 
-        //Define a function to use random method and removeAt method to randomize the order of objects in the list: 
-        //Syntax: accessModifier returnType dataType functionName(dataType parameter)
-        //returnType is static so that this method is called withouth instantiating an object.
-        public static Deck Shuffle(Deck deck)
-        {
-            //Create a temporary list to store shuffled objects:
-            List<Card> TempList = new List<Card>();
-            //Random method:
-            Random random = new Random();
+        ////METHOD 1
+        ////Define a function to use random method and removeAt method to randomize the order of objects in the list: 
+        ////Syntax: accessModifier returnType dataType functionName(dataType parameter)
+        ////returnType is static so that this method can be called withouth instantiating an object.
+        //public static Deck Shuffle(Deck deck)
+        //{
+        //    //Create a temporary list to store shuffled objects:
+        //    List<Card> TempList = new List<Card>();
+        //    //Random method:
+        //    Random random = new Random();
 
-            while (deck.Cards.Count > 0)
+        //    while (deck.Cards.Count > 0)
+        //    {
+        //        int randomIndex = random.Next(0, deck.Cards.Count);
+        //        TempList.Add(deck.Cards[randomIndex]);
+        //        deck.Cards.RemoveAt(randomIndex);
+        //    }
+        //    deck.Cards = TempList;
+        //    return deck;
+        //}
+
+        ////METHOD 2
+        ////Method Overloading: multiple methods with the same name, but different contents/parameters/substance
+        ////This method uses a loop to apply the Shuffle method to every iteration:
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i ++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
+
+        //METHOD 3
+        //Instead of using method overloading, assign a parameter with a default value
+        //Assign an out variable BEFORE optional parameters
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
+        {
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
+                timesShuffled++;
+                //Create a temporary list to store shuffled objects:
+                List<Card> TempList = new List<Card>();
+                //Random method:
+                Random random = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
             }
-            deck.Cards = TempList;
             return deck;
         }
     }
