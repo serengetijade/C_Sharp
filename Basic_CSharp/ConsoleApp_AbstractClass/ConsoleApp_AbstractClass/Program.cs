4726bde00f81acb22fb59e6f7fe43a031ee00b05
SerengetiJade <jad245@gmail.com>
2022-11-22 13:44:35 -0800

    Notes

--- Basic_CSharp/ConsoleApp_AbstractClass/ConsoleApp_AbstractClass/Program.cs
+++ Basic_CSharp/ConsoleApp_AbstractClass/ConsoleApp_AbstractClass/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp_AbstractClass
     {
         static void Main(string[] args)
         {
+            //Instantiate an Employee object:
             Employee instance1 = new Employee() { firstName = "Sample", lastName = "Student"};
+            //Call a (Employee) Class Medhod: 
             instance1.SayName();
             Console.ReadLine();
         }
