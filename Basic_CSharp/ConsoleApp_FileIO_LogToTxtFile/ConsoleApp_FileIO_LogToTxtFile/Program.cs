47bd2d7e74a677eb13463cd2eef7d16730eed8f0
SerengetiJade <jad245@gmail.com>
2022-11-30 16:58:36 -0800

    File IO StreamWriter to append to a text file.

---
+++ Basic_CSharp/ConsoleApp_FileIO_LogToTxtFile/ConsoleApp_FileIO_LogToTxtFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp_FileIO_LogToTxtFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string answer = Console.ReadLine();
+            Console.WriteLine($"You entered {answer}");
+            //Write each card delt to a txt file:`
+            using (StreamWriter file = new StreamWriter(@"C:\Users\jad24\Documents\Coding Projects\C_Sharp\TwentyOneGame\TwentyOneGame\log.txt", true))
+            {
+                file.WriteLine(answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOneGame/TwentyOneGame/Dealer.cs
+++ TwentyOneGame/TwentyOneGame/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -17,7 +18,13 @@ namespace TwentyOne
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());  //Use the First() method to retireve the first item of a list. Hand is a property of the Player class, it is a List consisting of <Card> type objects. Deck is a Deck type (defined in Deck.cs) property of Dealer. As a Deck type, it is a Deck class objeck with a property Cards, which is a List consisting of <Card> type objects. First is a method that retrieves the first object of that list.
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");     //Apply the overridden ToString method (defined in Card.cs).
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");    //Apply the overridden ToString method (defined in Card.cs).
+            Console.WriteLine(card);
+            //Write each card delt to a txt file:
+            using (StreamWriter file = new StreamWriter(@"C:\Users\jad24\Documents\Coding Projects\C_Sharp\TwentyOneGame\TwentyOneGame\log.txt", true))
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);  //Use RemoveAt() method to remove an object from a list at a given index location, in this case it is the zero index (the first) object that was 'dealt out'.
         }
     }
