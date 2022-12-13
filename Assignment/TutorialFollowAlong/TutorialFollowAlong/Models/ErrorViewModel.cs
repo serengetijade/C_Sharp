b1c8659ae39dcb3ea1ce3d8a9f385990267e1ac2
SerengetiJade <jad245@gmail.com>
2022-12-13 15:27:06 -0800

    Challenge 547

---
+++ Assignment/TutorialFollowAlong/TutorialFollowAlong/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TutorialFollowAlong.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
