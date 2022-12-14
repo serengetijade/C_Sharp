764aabe1e9839a0e06979ab313676e8992ac380a
SerengetiJade <jad245@gmail.com>
2022-12-13 17:56:03 -0800

    Added notes about spinning up a basic web app/website

---
+++ WebAppTutorial/WebAppTutorial/Pages/OtherPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppTutorial.Pages
+{
+    public class OtherPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
