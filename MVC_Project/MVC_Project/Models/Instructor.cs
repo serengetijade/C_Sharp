f0ea9a77602f436c7f6c922aa8b884b80dbd034d
SerengetiJade <jad245@gmail.com>
2022-12-13 19:32:37 -0800

    Added descriptions and removed basic scaffolding fluff.

--- MVC_Project/MVC_Project/Models/Instructor.cs
+++ MVC_Project/MVC_Project/Models/Instructor.cs
@@ -5,8 +5,10 @@ using System.Web;
 
 namespace MVC_Project.Models
 {
+    //Define a class
     public class Instructor
     {
+        //Define a Property Syntax: accessModifier Type propertyName { get; set; }  
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
