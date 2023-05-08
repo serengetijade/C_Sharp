e397085f3b02b1e4a7a46262ef107ecfbaba25d2
SerengetiJade <jad245@gmail.com>
2023-05-07 17:55:38 -0700

    FizzBuzz

--- Basic_CSharp/ConsoleApp_ArraysLists/ConsoleApp_ArraysLists/Program.cs
+++ Basic_CSharp/ConsoleApp_ArraysLists/ConsoleApp_ArraysLists/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic; //required for Lists
+using System.Threading;
 
 namespace ConsoleApp_ArraysLists
 {
@@ -126,6 +127,50 @@ namespace ConsoleApp_ArraysLists
                 }
             }
             Console.ReadLine();
+
+            //FizzBuzz 
+            //print number starting from 1 to 100.
+            //When a number is multiple of three, print “Fizz” instead of a number on the console and 
+            //if multiple of five then print “Buzz” on the console.
+            //For numbers which are multiple of three as well five, print “FizzBuzz” on the console.
+            //Method 1: FOR loop
+            for (int i = 1; i<=100; i++)
+            {
+                if (i%3 == 0 && i%5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            //Method 2: 
+            for (int i =1; i<=100; i++)
+            {
+                string iteration = "";
+                if (i % 3 == 0)
+                {
+                    iteration += "Fizz";
+                }
+                if (i%5==0)
+                {
+                    iteration += "Buzz";
+                }
+                if (iteration.Length ==0)
+                {
+                    iteration = i.ToString();
+                }
+                Console.WriteLine(iteration);
+            }
         }
     }
 }
