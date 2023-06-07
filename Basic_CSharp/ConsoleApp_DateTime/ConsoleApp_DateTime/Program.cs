c7852ccf2f2856aeaae4084ccc74b1521ff1dc92
SerengetiJade <jad245@gmail.com>
2023-06-07 15:28:47 -0700

    Format DateTime and UTC

--- Basic_CSharp/ConsoleApp_DateTime/ConsoleApp_DateTime/Program.cs
+++ Basic_CSharp/ConsoleApp_DateTime/ConsoleApp_DateTime/Program.cs
@@ -18,6 +18,20 @@ namespace ConsoleApp_DateTime
             //.Add method:
             Console.WriteLine($"In {input} hours it will be {dateTime.Add(timeSpan)}");
 
+            //Set a timespan for the future: 
+            TimeSpan timeSpan1 = new TimeSpan(1, 2, 0);
+            System.Diagnostics.Debug.WriteLine(timeSpan1.Hours);  //Log the number of Hours/Minutes/Seconds to the debug terminal.
+
+            //Format DateTime
+            Console.WriteLine("Format the DateTime using .ToString(values):");
+            Console.WriteLine(DateTime.Now.ToString("dddd, M/dd/yyyy h:mm:ss tt"));
+
+            //UTC (formerly GMT)
+            DateTime dateTimeUTC = DateTime.UtcNow;
+            DateTime dateTimeUTCToLocal = DateTime.UtcNow.ToLocalTime();
+            DateTime dateTimeLocalToUTC = DateTime.Now.ToUniversalTime();
+            Console.WriteLine("The UTC time is: " + dateTimeUTC.ToString("MM/dd/yy HH:mm:ss") + " " + dateTimeUTC.Kind);
+
             Console.ReadLine();
         }
     }
