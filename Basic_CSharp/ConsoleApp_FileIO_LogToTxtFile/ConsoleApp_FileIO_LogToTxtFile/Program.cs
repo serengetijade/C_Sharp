64378c53e1ae9c3f987c9778375dd357cf22993e
SerengetiJade <jad245@gmail.com>
2023-06-11 13:48:06 -0700

    Create a new file

--- Basic_CSharp/ConsoleApp_FileIO_LogToTxtFile/ConsoleApp_FileIO_LogToTxtFile/Program.cs
+++ Basic_CSharp/ConsoleApp_FileIO_LogToTxtFile/ConsoleApp_FileIO_LogToTxtFile/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using System.IO; //Contains methods needed for working with files. 
 
 namespace ConsoleApp_FileIO_LogToTxtFile
 {
@@ -7,15 +7,34 @@ namespace ConsoleApp_FileIO_LogToTxtFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number:");
-            string answer = Console.ReadLine();
-            Console.WriteLine($"You entered {answer}");
-            //Write each card delt to a txt file:`
-            using (StreamWriter file = new StreamWriter(@"C:\Users\jad24\Documents\Coding Projects\C_Sharp\TwentyOneGame\TwentyOneGame\log.txt", true))
+            //Console.WriteLine("Please enter a line to append to the file:");
+            //string answer = Console.ReadLine();
+            //Console.WriteLine($"You entered: \"{answer}\"");
+
+            ////StreamWriter: write user input to a (.txt) file:`
+            //using (StreamWriter file = new StreamWriter(@"C:\Users\jad24\Documents\CodingProjects\ActiveProjects\log.txt", true))
+            //{
+            //    //file.WriteLine(answer);
+            //    file.WriteLine(DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt") + ": " + answer);     //Automatically writes to a new line. 
+            //}
+            //Console.ReadLine();         ;
+
+            //Create a new file:
+            Console.WriteLine("Enter a file name:");
+            string filename = Console.ReadLine();
+            //Note: in the string path, use double \\ to avoid escape character mixup
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + $"\\CodingProjects\\ActiveProjects\\{filename}.txt";
+            //Check if file exists so as to NOT overwrite it:
+            if (!File.Exists(path))
             {
-                file.WriteLine(answer);
+                File.Create(path);
+                Console.WriteLine("A new file was created."); 
             }
-            Console.ReadLine();
+            else
+            {
+                Console.WriteLine("That filename already exists.");
+            }
+
         }
     }
 }
--- README.md
+++ README.md
@@ -67,6 +67,7 @@ In addition to the projects, there is a small code library for referencing basic
 - [Events and Delegates](https://github.com/serengetijade/C_Sharp/tree/main/Basic_CSharp/ConsoleApp_EventsAndDelegates) An event is an action that occurs as the result of user action. Here is an example of event and event handler syntax, popular naming convention with events, and custom EventArgs classes.
 - [Exception Handling](https://github.com/serengetijade/C_Sharp/tree/main/Basic_CSharp/ConsoleApp_ExceptionHandling/ConsoleApp_ExceptionHandling) Try catch blocks and exception codes.
 - [FileIO](https://github.com/serengetijade/C_Sharp/blob/main/Basic_CSharp/ConsoleApp_FileIO_LogToTxtFile/ConsoleApp_FileIO_LogToTxtFile/Program.cs) The FileIO library and how to log to text files. 
+- [Input Validation]() Check that user input is type int. 
 - [Lambda](https://github.com/serengetijade/C_Sharp/tree/main/Basic_CSharp/ConsoleApp_Lambda/ConsoleApp_Lambda) Using lambda expressions, you can write local functions that can be passed as arguments or returned as the value of function calls.
 - [Key and Console Modifiers](https://github.com/serengetijade/C_Sharp/tree/main/Basic_CSharp/ConsoleApp_Key_Console_Modifiers) Log to the console when key modifiers are pressed. 
 - [Methods](https://github.com/serengetijade/C_Sharp/tree/main/Basic_CSharp/ConsoleApp_Methods/ConsoleApp_Methods) The basics of C# methods. 
