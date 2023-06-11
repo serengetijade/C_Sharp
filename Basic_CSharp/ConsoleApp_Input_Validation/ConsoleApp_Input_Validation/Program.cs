f8c0d80b830a5d7e538dcd27884f25682a54d67e
SerengetiJade <jad245@gmail.com>
2023-06-10 17:09:22 -0700

    Check that user input is a number

---
+++ Basic_CSharp/ConsoleApp_Input_Validation/ConsoleApp_Input_Validation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Input_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check that user input is a number:
+            Console.WriteLine("Please enter a number.");
+            do
+            {
+                string userInput = Console.ReadLine();
+                int number;
+
+                //Syntax: int.TryParse(variable to attempt to convert to an int, int variable to store the number if the call succeeds).
+                if (!int.TryParse(userInput,out number))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You did not enter a valid input.");
+                    Console.ResetColor();
+                    Console.WriteLine("Please enter a number.");
+                }
+                else
+                {
+                    Console.WriteLine("Thank you. Please enter another number.");
+                }
+
+            } while (true);
+        }
+    }
+}
