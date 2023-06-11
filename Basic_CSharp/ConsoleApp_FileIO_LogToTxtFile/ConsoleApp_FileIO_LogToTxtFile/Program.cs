bc5567dad9f141e7d6cb7890b3c21dd3da5bdccc
SerengetiJade <jad245@gmail.com>
2023-06-11 14:23:12 -0700

    FilesStream and convert strings to byte array.

--- Basic_CSharp/ConsoleApp_FileIO_LogToTxtFile/ConsoleApp_FileIO_LogToTxtFile/Program.cs
+++ Basic_CSharp/ConsoleApp_FileIO_LogToTxtFile/ConsoleApp_FileIO_LogToTxtFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO; //Contains methods needed for working with files. 
+using System.IO;    //Contains methods needed for working with files. 
+using System.Text;  //Methods for UTF8Encoding.
 
 namespace ConsoleApp_FileIO_LogToTxtFile
 {
@@ -7,19 +8,8 @@ namespace ConsoleApp_FileIO_LogToTxtFile
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Please enter a line to append to the file:");
-            //string answer = Console.ReadLine();
-            //Console.WriteLine($"You entered: \"{answer}\"");
 
-            ////StreamWriter: write user input to a (.txt) file:`
-            //using (StreamWriter file = new StreamWriter(@"C:\Users\jad24\Documents\CodingProjects\ActiveProjects\log.txt", true))
-            //{
-            //    //file.WriteLine(answer);
-            //    file.WriteLine(DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt") + ": " + answer);     //Automatically writes to a new line. 
-            //}
-            //Console.ReadLine();         ;
-
-            //Create a new file:
+            //CREATE A NEW FILE:
             Console.WriteLine("Enter a file name:");
             string filename = Console.ReadLine();
             //Note: in the string path, use double \\ to avoid escape character mixup
@@ -35,6 +25,30 @@ namespace ConsoleApp_FileIO_LogToTxtFile
                 Console.WriteLine("That filename already exists.");
             }
 
+            //STREAMWRITER: Append a string to a file using StreamWriter:
+            Console.WriteLine("Please enter a line to append to the file:");
+            string answer1 = Console.ReadLine();
+            Console.WriteLine($"You entered: \"{answer1}\"");
+            //StreamWriter: write user input to a (.txt) file:`
+            //Syntax: using (StreamWriter file = new StreamWriter(@"C:\path\path\fileName.txt", true))
+            using (StreamWriter file = new StreamWriter($"C:\\Users\\jad24\\Documents\\CodingProjects\\ActiveProjects\\{filename}.txt", true))
+            {
+                //file.WriteLine(answer);
+                file.WriteLine(DateTime.Now.ToString("\nMM/dd/yyyy h:mm:ss tt") + ": " + answer1);     //Automatically writes to a new line. 
+            }
+
+            //FILESTREAM: Append text to a file using FileStream:
+            Console.WriteLine("Please enter another line to append to the file:");
+            string answer2 = DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt") + ": " + Console.ReadLine();
+            //Note: FileStream adds via a byte array, so strings must be converted to byte arrays:
+            byte[] infoToAdd = new UTF8Encoding(true).GetBytes(answer2);
+            //New FileStream Syntax: FileStream varName = File.Open(path, FileMode.methodName); 
+            FileStream fs = File.Open(path, FileMode.Append);
+            //Syntax: FileStreamInstanceName.Write(byteArray varName, offset, limit for lenght of the text to write to the file);   
+            fs.Write(infoToAdd, 0, infoToAdd.Length);
+            fs.Close();
+
+            Console.ReadLine();
         }
     }
 }
