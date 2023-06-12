0d5ef96c9eea7c1502a117d1c7d8b2475617b883
SerengetiJade <jad245@gmail.com>
2023-06-12 16:43:28 -0700

    Attributes and custom attribute code

---
+++ Basic_CSharp/ConsoleApp_Attributes/ConsoleApp_Custom_Attribute/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+
+namespace Custom_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Obsolete Attribute:
+            [Obsolete("OldMethod is obsolete.Please use NewMethod instead")]
+            static void OldMethod() { };
+            static void NewMethod() { };
+
+            ////Conditional Attribute: 
+            //[Conditional("Condition1 example text")]
+            //void ExampleMethod() { };
+
+        }
+    }
+}
+
+//Custom Attribute:
+[AttributeUsage(AttributeTargets.All)]      //This attribute is valid for all targets. 
+public class ExampleAttribute : Attribute { }
+
+class ExampleClass
+{
+    [ExampleAttribute]
+    private object exampleObj;
+}
