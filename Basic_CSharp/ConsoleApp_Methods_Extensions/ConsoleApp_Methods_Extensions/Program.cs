cdc2c4d01f0e71793b83495e990fe7dd9edc8687
SerengetiJade <jad245@gmail.com>
2023-06-12 19:51:37 -0700

    Extension Methods

---
+++ Basic_CSharp/ConsoleApp_Methods_Extensions/ConsoleApp_Methods_Extensions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    /// <summary>
+    /// Extensions must be defined in a non-generic static class:
+    /// They add methods to existing and/or built-in classes. 
+    /// </summary>
+    public static class Program
+    {
+        static bool ExtensionMethod_GreaterThan(this int i, int val)
+        {
+            if (i > val) return true;
+            else return false;
+        }
+        static void Main(string[] args) 
+        { 
+            //Extension methods: 
+            bool ext1 = 2.ExtensionMethod_GreaterThan(1);
+            Console.WriteLine(ext1);
+        }
+    }
+}
