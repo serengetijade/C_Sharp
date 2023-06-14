40b12dd975c0e532280ebd78100496c619d6f97a
SerengetiJade <jad245@gmail.com>
2023-06-14 11:24:30 -0700

    DbContext with EntityFrameworkCore

---
+++ Project_SWAPI/Project_SWAPI/Models/SWAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_SWAPI.Models
+{
+    public class SWAPIContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<People> People { get; set; }
+        public DbSet<Planet> Planets { get; set; }
+        public DbSet<Species> Species { get; set; }
+        public DbSet<Starship> Starships { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
--- Project_SWAPI/Project_SWAPI/Models/SWAPIModelsContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Project_SWAPI.Models
-{
-    public class SWAPIModelsContext
-    {
-    }
-}
