0b016dea0f61c942859e76cd43d7bd85924ea1c6
SerengetiJade <jad245@gmail.com>
2023-06-25 14:03:04 -0700

    add enum methods: GetNames, GetValues

--- Basic_CSharp/ConsoleApp_Enums/ConsoleApp_Enums/Day.cs
+++ Basic_CSharp/ConsoleApp_Enums/ConsoleApp_Enums/Day.cs
@@ -5,7 +5,9 @@ using System.Text;
 namespace ConsoleApp_Enums
 {
     //Best practice is to define enums in their own document.
-    //Declare a Enum Type Syntax: accessModifier enum enumName { enumerator1, enumerator2, etc }
+    //Declare a Enum Type Syntax:
+    //accessModifier enum enumName { enumerator1, enumerator2, etc }
+    //Note: items in an enum are called enumerators or elements. 
     public enum DaysOfWeek
     {
         Monday, Tuesday,Wednesday,Thursday, Friday, Saturday, Sunday
--- Basic_CSharp/ConsoleApp_Enums/ConsoleApp_Enums/Program.cs
+++ Basic_CSharp/ConsoleApp_Enums/ConsoleApp_Enums/Program.cs
@@ -4,20 +4,40 @@ namespace ConsoleApp_Enums
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            
+            methodName();
+            //public enum DaysOfWeek = new Enum { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
             Console.WriteLine("Enter the current day of the Week");
-            try 
+            try
             {
                 string readDay = Console.ReadLine();
-                DayOfWeek day = (DayOfWeek) Enum.Parse(typeof(DayOfWeek), readDay);
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), readDay);
                 Console.WriteLine(day);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Please enter an actual day of the week");
             }
+        }
+
+        //Enum examples with built in methods: 
+        enum ExampleEnum { Element1 = 5, Element2, Element3, Element4 };
+
+        public static void methodName()
+        {
+        Console.WriteLine(Enum.GetName(typeof(ExampleEnum), 6));    //Result: 'Element2' because Element1 has a value of 5. 
+
+        //Get the name of each element in an enum: 
+        foreach(string s in Enum.GetNames(typeof(ExampleEnum)))
+            {
+                Console.WriteLine(s);
+            }
+
+        //Get the value of each element in an enum: 
+        foreach (int i in Enum.GetValues(typeof(ExampleEnum))) { Console.WriteLine(i); }
         }
     }
 }
