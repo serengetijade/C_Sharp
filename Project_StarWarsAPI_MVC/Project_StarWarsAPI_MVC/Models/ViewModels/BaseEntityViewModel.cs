80cd91cdb10d269edaa0e73de80b96f1385dee1a
SerengetiJade <jad245@gmail.com>
2024-06-23 19:07:37 -0700

    Change to csv.

--- Project_StarWarsAPI_MVC/Project_StarWarsAPI_MVC/Models/ViewModels/BaseEntityViewModel.cs
+++ Project_StarWarsAPI_MVC/Project_StarWarsAPI_MVC/Models/ViewModels/BaseEntityViewModel.cs
@@ -8,7 +8,7 @@ namespace Project_StarWarsAPI_MVC.Models.Swapi
         public int Id { get; set; }
         public string Created { get; set; }
         public string Edited { get; set; }
-        public string[]? Films { get; set; }
+        public string Films { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
     }
