77c87073f064a6a8d3cc3d18c11edb6c9c7b39c7
Jade Abreu <110346143+serengetijade@users.noreply.github.com>
2025-08-28 17:03:43 -0600

    SendRequest (getAll) option.

--- SwapiWebApp/SwapiWebApp/Repositories/SwapiRepository.cs
+++ SwapiWebApp/SwapiWebApp/Repositories/SwapiRepository.cs
@@ -1,5 +1,7 @@
 ﻿using SwapiWebApp.Repositories.Models;
 using SwapiWebApp.Services.Models;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace SwapiWebApp.Repositories
 {
@@ -24,24 +26,43 @@ namespace SwapiWebApp.Repositories
 			_httpClient = httpClientFactory.CreateClient("SwapiClient");
         }
 
-        public async Task<ServiceResult<ApiResult>> SendRequest(string endpoint)
+        public async Task<ServiceResult<List<T>>> SendRequest<T>(string endpoint, bool getAllPages = false)
         {
             try { 
-                var response = await _httpClient.GetAsync($"{endpoint}/");
+                var jsonSerializerOptions = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+				var response = await _httpClient.GetAsync($"{endpoint}/");
                 response.EnsureSuccessStatusCode();
                 var content = await response.Content.ReadAsStringAsync();
 
-                var result = System.Text.Json.JsonSerializer.Deserialize<ApiResult>(content, 
-                    new System.Text.Json.JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    }
-                );
-				return new ServiceResult<ApiResult>(true, "Request successful", result);
-            }
+                var allResults = new List<JsonObject>();
+                var result = JsonSerializer.Deserialize<ApiResult>(content, jsonSerializerOptions);
+
+                allResults.AddRange(result != null ? result.Results : new ());
+
+				while (getAllPages && result != null && !string.IsNullOrWhiteSpace(result.Next))
+                {
+                    var nextResponse = await _httpClient.GetAsync(result.Next);
+                    nextResponse.EnsureSuccessStatusCode();
+                    var nextContent = await nextResponse.Content.ReadAsStringAsync();
+                    if (string.IsNullOrWhiteSpace(nextContent))
+                        break;
+					var nextResult = JsonSerializer.Deserialize<ApiResult>(nextContent, jsonSerializerOptions) ?? new ();
+                    allResults.AddRange(nextResult.Results);
+                    result.Next = nextResult != null ? nextResult.Next : string.Empty;
+				}
+
+                var finalResults = JsonSerializer.Deserialize<List<T>>(
+                    JsonSerializer.Serialize(allResults), jsonSerializerOptions) ?? new ();
+
+                return new ServiceResult<List<T>>(true, "Request successful", finalResults);
+			}
             catch (HttpRequestException ex)
             {
-                return new ServiceResult<ApiResult>(false, $"Request failed: {ex.Message}", null);
+                return new ServiceResult<List<T>>(false, $"Request failed: {ex.Message}", new List<T>());
 			}
         }
 	}	
